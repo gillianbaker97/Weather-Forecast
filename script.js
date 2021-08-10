772c2be85dd1e187374cd592cb86ca51cc9bdcb4
Gillian Baker <gillianbaker97@gmail.com>
2021-08-09 23:00:45 -0400

    updating the api calls

--- script.js
+++ script.js
@@ -1,39 +1,63 @@
 
 var current = document.querySelector('.card-deck');
 var searchButton = document.querySelector('.form-control');
-var weatherCard = document.querySelector('.weather-card');
+var currentWeather = document.querySelector('#current-temp');
 var repoForecast = document.querySelector('.forecast');
 
 
 
+function todayWeather() {
+  var userSearch = document.getElementById('user-search').value;
+  console.log(userSearch);
+  var apiToday = 'api.openweathermap.org/data/2.5/weather?q=' + userSearch + '&appid=2cb07c625e5379f83f22d88720054104';
+  console.log(apiToday)
+
+  fetch(apiToday)
+    .then(console.log(response.json));
+
+};
+
+document.getElementById('searchButton').addEventListener('click', todayWeather);
+ 
+
+function nextWeather () {
+  var userSearch = document.getElementById('user-search').value;
+  console.log(userSearch);
+  var apiNext = 'api.openweathermap.org/data/2.5/forecast?q=' + userSearch + '&appid=2cb07c625e5379f83f22d88720054104';
+  console.log(apiNext);
+
+  fetch(apiNext)
+    .then(console.log(response.json));
+};
+
+document.getElementById('searchButton').addEventListener('click', nextWeather);
+
+
 var formSubmitHandler = function(event){
-      event.preventDefault();
-
-      var city = searchButton.value.trim(); /*trims white space from the input button*/
-
-      if (city) {
-          getUserRepos(city);
-        
-          weatherCard.textContent = '';/* asks the weather card to display the correct city once inputted */
-
-         } else {
-            alert('Please enter a city name'); /* if the city isn't recognized in the repo's we ask for another input*/
-         }
+  event.preventDefault();
+  var city = userSearch.value.trim(); /*trims white space from the input button*/
+  localStorage.setItem("city");
   };
 
-var foreCast = function (event) {
+var displayCity = function(event){
+  event.preventDefault();
+  var city = userSearch.value.trim(); /*trims white space from the input button*/
+  localStorage.getItem("city") = document.getElementById('search-result');
+}
+
+/*  var foreCast = function (event) {
     var forecast = event.target.getAttribute('.forecast'); /* gets the forecast for each day of the week */
 
-    if (forecast) {
+    /* if (forecast) {
         getFeaturedRepos(forecast);
 
         weatherCard.textContent = '';
     }
-};
+}; */
 
-var getUserRepos = function (user) {
-    var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat={40.95}&lon={-74.07}&exclude={part}&appid={2cb07c625e5379f83f22d88720054104}';
-
+/* var getUserRepos = function (user) {
+    var apiUrl = 'api.openweathermap.org/data/2.5/weather?lat=40.95&lon=-74.07&appid=2cb07c625e5379f83f22d88720054104';
+    //var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat={40.95}&lon={-74.07}&exclude={part}&appid={2cb07c625e5379f83f22d88720054104}';
     fetch(apiUrl)
         .then(function(response){
             if(response.ok){
@@ -47,11 +71,12 @@ var getUserRepos = function (user) {
         .catch(function(error){
             alert('Unable to connect to Weather');
         });
-};
+}; 
 
 
 var getForecastRepos = function(language) {
-    var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat={40.95}&lon={-74.07}&exclude={part}&appid={2cb07c625e5379f83f22d88720054104}';
+  var apiUrl = 'api.openweathermap.org/data/2.5/weather?lat=40.95&lon=-74.07&appid=2cb07c625e5379f83f22d88720054104';
+
     
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
@@ -63,21 +88,22 @@ var getForecastRepos = function(language) {
         }
       });
     };
-
+*/
 
 var displayForecast = function (repos, forecast) {
     if (repos.length === 0) {
       weatherCard.textContent = 'No repositories found.';
       return;
-    }
+    };
 
     weatherCard.textContent = forecast;
 
     for (var i = 0; i < data.length; i++){
+
     var container = document.querySelector('.container');
-    var temp = document.querySelector('temp');
-    var wind = document.querySelector('wind');
-    var humidity = document.querySelector('humidity');
+    var temp = document.querySelector('.temp');
+    var wind = document.querySelector('.wind');
+    var humidity = document.querySelector('.humidity');
 
     link.textContent = data[i].html.url;
     link.href = data[i].html.url;
@@ -92,5 +118,5 @@ var displayForecast = function (repos, forecast) {
 
                                  
 
-searchButton.addEventListener('click', apiUrl);
+//searchButton.addEventListener('click', apiUrl);
 
